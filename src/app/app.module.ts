9f5335dcb84c3ea262993467b05eab9c26b98e9d
Mouhamet99 <dieyemouhamet1@gmail.com>
2023-01-12 16:29:59 +0000

    add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,34 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HomeComponent } from './student/home/home.component';
-import { CoursesComponent } from './student/courses/courses.component';
+import { HomeComponent } from './students/home/home.component';
+import { CoursesComponent } from './students/courses/courses.component';
+import { PathsComponent } from './students/paths/paths.component';
+import { PricingComponent } from './students/pricing/pricing.component';
+import { TeachersComponent } from './students/teachers/teachers.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SidebarComponent,
-    CoursesComponent
+    CoursesComponent,
+    PathsComponent,
+    PricingComponent,
+    TeachersComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      {path:'student',component: HomeComponent  }
+      {path:'',component: HomeComponent  },
+      {path:'home',component: HomeComponent  },
+      {path:'courses',component: CoursesComponent  },
+      {path:'paths',component: PathsComponent  },
+      {path:'pricing',component: PricingComponent  },
+      {path:'teachers',component: TeachersComponent  },
+      {path: '**', component: NotFoundComponent}
     ])
   ],
   providers: [],
--- src/app/students/paths/paths.component.spec.ts
+++ src/app/students/paths/paths.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SidebarComponent } from './sidebar.component';
+import { PathsComponent } from './paths.component';
 
-describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
+describe('PathsComponent', () => {
+  let component: PathsComponent;
+  let fixture: ComponentFixture<PathsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
+      declarations: [ PathsComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SidebarComponent);
+    fixture = TestBed.createComponent(PathsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
